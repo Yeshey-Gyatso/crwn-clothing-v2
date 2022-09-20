c8dfb58836f6cfc4f904e4ce13caeecfe20d5ce0
Yeshey-Gyatso <yesheygyastoo.yg@gmail.com>
2022-09-20 12:21:32 +0530

    updating our directory page with directory component and category item component

---
+++ crwn-clothing/src/components/categories-menu/catergories-menu.components.jsx
@@ -0,0 +1,22 @@
+import './categories-menu.components.scss';
+import CategoryItem from '../category-item/category-item.component';
+
+
+const CategoryMenu = ({categories}) =>{
+
+    
+      return (
+        <div className='categories-container'>
+      {categories.map((category)=>(
+       <CategoryItem key={category.id} category={category} />
+        ))
+      }
+      
+      </div>
+
+
+      );
+
+
+}
+export default CategoryMenu
